b2e2772907cdfbc094b07c63c2e9957711259251
Bobby Kuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2023-06-13 19:03:21 +0300

    Update 2_the_lift.py

--- L06_Mid_Exam/2_the_lift.py
+++ L06_Mid_Exam/2_the_lift.py
@@ -1,24 +1,47 @@
-people_waiting = int(input())
-lift_state = list(map(int, input().split()))
+def find_place_in_lift(people, lift_state):
+    lift_state = list(map(int, lift_state.split()))
+    total_capacity = len(lift_state) * 4
+    remaining_people = people
 
-total_wagons = len(lift_state)
-current_wagon = 0
+    for i in range(len(lift_state)):
+        while lift_state[i] < 4 and remaining_people > 0:
+            lift_state[i] += 1
+            remaining_people -= 1
 
-while people_waiting > 0 and current_wagon < total_wagons:
-    available_space = 4 - lift_state[current_wagon]
-    if available_space >= people_waiting:
-        lift_state[current_wagon] += people_waiting
-        people_waiting = 0
+    if remaining_people > 0:
+        return f"There isn't enough space! {remaining_people} people in a queue!\n{' '.join(map(str, lift_state))}"
+    elif sum(lift_state) < total_capacity:
+        return f"The lift has empty spots!\n{' '.join(map(str, lift_state))}"
     else:
-        lift_state[current_wagon] += available_space
-        people_waiting -= available_space
-    current_wagon += 1
+        return ' '.join(map(str, lift_state))
 
-if people_waiting == 0 and 0 in lift_state:
-    print("The lift has empty spots!")
-    print(" ".join(map(str, lift_state)))
-elif people_waiting > 0 and current_wagon == total_wagons:
-    print(f"There isn't enough space! {people_waiting} people in a queue!")
-    print(" ".join(map(str, lift_state)))
-else:
-    print(" ".join(map(str, lift_state)))
+
+# Example usage
+people = int(input())
+lift_state = input()
+
+result = find_place_in_lift(people, lift_state)
+print(result)
+
+# Description #
+# Write a program that finds a place for the tourist on a lift.
+# Every wagon should have a maximum of 4 people on it. If a wagon is full, you should direct the people to the next
+# one with space available.
+# Input
+#   • On the first line, you will receive how many people are waiting to get on the lift
+#   • On the second line, you will receive the current state of the lift separated by a single space: " ".
+# Output
+#   When there is no more available space left on the lift, or there are no more people in the queue, you should print
+# on the console the final state of the lift's wagons separated by " " and one of the following messages:
+# • If there are no more people and the lift have empty spots, you should print:
+#   "The lift has empty spots!
+#   {wagons separated by ' '}"
+# • If there are still people in the queue and no more available space, you should print:
+# "There isn't enough space! {people} people in a queue!
+# {wagons separated by ' '}"
+# • If the lift is full and there are no more people in the queue, you should print only the wagons
+# separated by " "
+# Input: 15
+# Output: 0 0 0 0
+# Input: 20
+# Output: 0 2 0
