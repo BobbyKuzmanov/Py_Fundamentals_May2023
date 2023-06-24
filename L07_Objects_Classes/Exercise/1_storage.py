e3105d5fcdc130b9b541130dc3e65a71c8a4ce39
Bobby Kuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2023-06-24 14:55:38 +0300

    Add files via upload

---
+++ L07_Objects_Classes/Exercise/1_storage.py
@@ -0,0 +1,37 @@
+class Storage:
+    storage = []
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+    def add_product(self, product):
+        if len(self.storage) < self.capacity:
+            self.storage.append(product)
+
+    def get_products(self):
+        return self.storage
+
+
+storage = Storage(4)
+storage.add_product("apple")
+storage.add_product("banana")
+storage.add_product("potato")
+storage.add_product("tomato")
+storage.add_product("bread")
+print(storage.get_products())
+
+# Description #
+# Create a class Storage. The __init__ method should accept one parameter - the capacity of the storage.
+# The Storage class should also have an attribute called storage - empty list, where all the items will be stored.
+# The class should have two additional methods:
+#   • add_product(product: str) - adds the product in the storage if there is enough space for it
+#   • get_products() - returns the storage list
+#
+# Input                                 Output #
+# storage = Storage(4)                  ["apple", "banana", "potato", "tomato"]
+# storage.add_product("apple")
+# storage.add_product("banana")
+# storage.add_product("potato")
+# storage.add_product("tomato")
+# storage.add_product("bread")
+# print(storage.get_products())
---
+++ L07_Objects_Classes/Exercise/2_weapon.py
@@ -0,0 +1,32 @@
+class Weapon:
+
+# Descriptions:
+# Create a class Weapon. The __init__ method should receive a number of bullets (integer).
+# Create an attribute called bullets to store that number.
+# The class should also have the following methods:
+#   • shoot()
+# o If there are bullets in the weapon, reduce them by 1 and return a message "shooting..."
+# o If there are no bullets left, return: "no bullets left"
+#   • __repr__()
+# o Returns "Remaining bullets: {amount_of_bullets}"
+
+#    Input
+# weapon = Weapon(5)
+# print(weapon.shoot())
+# print(weapon.shoot())
+# print(weapon)
+# print(weapon.shoot())
+# print(weapon.shoot())
+# print(weapon.shoot())
+# print(weapon.shoot())
+# print(weapon)
+
+#   Output
+# shooting...
+# shooting...
+# Remaining bullets: 3
+# shooting...
+# shooting...
+# shooting...
+# no bullets left
+# Remaining bullets: 0
